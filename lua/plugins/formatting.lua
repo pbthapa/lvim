5d7627e64370a5a81b7686fabdb840a1b0737b64
pbthapa <ptpbthapa24@gmail.com>
2023-10-21 22:52:34 +0545

    linting and formatter

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    {
+      "<leader>f",
+      mode = { "n", "v" },
+      desc = "Format this file",
+      function()
+        require("conform").format({
+          lsp_fallback = true,
+          async = false,
+          timeout_ms = 500,
+        })
+      end,
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      lua = { "stylua" },
+      python = { "isort", "black" },
+    },
+  },
+}
---
+++ lua/plugins/mason.lua
@@ -0,0 +1,47 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "whoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    require("mason-lspconfig").setup({
+      automatic_installation = true,
+      ensure_installed = {
+        "lua_ls",
+        "bashls",
+        "angularls",
+        "cssls",
+        "dockerls",
+        "html",
+        "tsserver",
+        "emmet_ls",
+        "jsonls",
+        "pyright",
+        "sqlls",
+        "yamlls",
+      },
+    })
+
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "isort",
+        "black",
+        "pylint",
+        "eslint_d",
+      },
+    })
+  end,
+}
